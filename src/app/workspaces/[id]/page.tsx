f85350a89d0f1f68a56d3a54171b6e6f69d4fe63
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-04-17 17:00:45 -0300

    feat: adding Header component to /workspaces layout.

---
+++ src/app/workspaces/[id]/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import { IconLayoutDashboard } from "@tabler/icons-react";
+
+
+export default function Tasks({ params }: { params: Promise<{ id: string }> }) 
+{
+    return (
+        <></>
+    );
+}
---
+++ src/components/layout/sections/header/header.tsx
@@ -0,0 +1,25 @@
+import { IconLayoutDashboard } from "@tabler/icons-react";
+
+import Logo from "@/components/tasking/logo";
+import * as NavBar from "@/components/navbars/navbar";
+
+
+export default function Header() 
+{
+    return (
+        <header 
+            className="
+                dark w-full flex justify-between items-center p-[1.25em] px-[2.5em] 
+                border-b-[1px] bg-background text-foreground
+            "
+        >
+            <Logo />
+            <NavBar.Root>
+                <NavBar.Route href="/workspaces">
+                    <IconLayoutDashboard stroke={1.5} className="size-[1.5rem]" />
+                    Workspace
+                </NavBar.Route>
+            </NavBar.Root>
+        </header>
+    );
+}
